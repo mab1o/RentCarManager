ed70f049ccdbb6e8f9ff5ce314696304f78284c8
Margaux Blondel <margaux.blondel@free.fr>
2024-04-01 15:54:07 +0200

    feat(servlet): add servlet for the rents list

---
+++ src/main/java/com/epf/rentmanager/servlet/ReservationListServlet.java
@@ -0,0 +1,27 @@
+package com.epf.rentmanager.servlet;
+
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/rents")
+public class ReservationListServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/views/rents/list.jsp").forward(request, response);
+    }
+}
